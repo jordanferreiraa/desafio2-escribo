6260112d616b17a4ac8cd6b4f23e769454290e60
jordanferreiraa <jordanferreira69@gmail.com>
2023-11-22 14:59:39 -0300

    feat: outputs no formato correto e JWT como token

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
+// Senha utilizada para assinatura digital
+const SECRET = 'jotaefe';
+
 // Simulando um armazenamento em memória
 const users = [];
+
+// Função para obter a data atual em formato string
+const getCurrentDateTime = () => new Date().toISOString();
 
 // Endpoint de cadastro (sign up)
 app.post('/signup', (req, res) => {
@@ -20,11 +28,31 @@ app.post('/signup', (req, res) => {
     return res.status(400).json({ error: 'E-mail já existente' });
   }
 
+  // Token JWT
+  const chave = jwt.sign({id: uuidv4()}, SECRET, { expiresIn: 1800 });
+
   // Cria um novo usuário
-  const newUser = { nome, email, senha, telefone };
+  const newUser = {
+    id: uuidv4(),
+    nome,
+    email,
+    senha,
+    telefone,
+    data_criacao: getCurrentDateTime(),
+    data_atualizacao: getCurrentDateTime(),
+    ultimo_login: null, // Definido como nulo inicialmente
+    token: chave
+  };
+
   users.push(newUser);
 
-  return res.status(201).json(newUser);
+  return res.status(201).json({
+    id: newUser.id,
+    data_criacao: newUser.data_criacao,
+    data_atualizacao: newUser.data_atualizacao,
+    ultimo_login: newUser.ultimo_login,
+    token: newUser.token,
+  });
 });
 
 // Endpoint de autenticação (sign in)
@@ -38,21 +66,42 @@ app.post('/signin', (req, res) => {
     return res.status(401).json({ error: 'Usuário e/ou senha inválidos' });
   }
 
-  return res.json({ message: 'Autenticação bem-sucedida' });
+  // Atualiza a última data de login
+  user.ultimo_login = getCurrentDateTime();
+
+  return res.json({
+    id: user.id,
+    data_criacao: user.data_criacao,
+    data_atuzalizacao: user.data_atuzalizacao,
+    ultimo_login: user.ultimo_login,
+    token: user.token
+  });
 });
 
 // Endpoint de recuperação de informações do usuário
 app.get('/user/:email', (req, res) => {
   const { email } = req.params;
+  const token = req.headers.authorization; // Pega o token do cabeçalho
 
   // Verifica se o usuário existe
   const user = users.find(user => user.email === email);
 
   if (!user) {
-    return res.status(404).json({ error: 'Não autorizado' });
+    return res.status(404).json({ error: 'Não autorizadooo' });
   }
 
-  return res.json(user);
+  // Verifica se o token no cabeçalho é válido
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
+    
+    // if (decoded.id !== user.id) {
+    //   return res.status(401).json({ error: 'Não autorizado', decoded: decoded.id, user: user.id });
+    // }
+
+    return res.json(user);
+  });
 });
 
 // Tratamento de endpoint não encontrado
